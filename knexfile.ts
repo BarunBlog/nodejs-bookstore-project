58c5cbe1151a3be422afde790e76d56bfd9a2ffa
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-07-10 20:04:16 +0600

    Added knexfile connection pool

--- knexfile.ts
+++ knexfile.ts
@@ -1,56 +1,59 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "pg",
+    client: 'pg',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      charset: "utf8",
+      charset: 'utf8',
+    },
+    pool: {
+      min: 2, // Minimum number of connections in the pool
+      max: 10, // Maximum number of connections in the pool
     },
     migrations: {
-      directory: __dirname + "/knex/migrations",
+      directory: __dirname + '/knex/migrations',
     },
     seeds: {
-      directory: __dirname + "/knex/seeds",
+      directory: __dirname + '/knex/seeds',
     },
   },
 
   staging: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      tableName: 'knex_migrations',
     },
   },
 
   production: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      tableName: 'knex_migrations',
     },
   },
 };
